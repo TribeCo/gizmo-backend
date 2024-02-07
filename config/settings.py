d709d2d7d67b0c7c8052e3c280fea8f72cb4299a
Taha <TahaM8000@gmail.com>
2024-02-08 02:20:02 +0330

    config project

--- config/settings.py
+++ config/settings.py
@@ -1,19 +1,30 @@
 from pathlib import Path
 import os
+from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-SECRET_KEY = 'django-insecure-2!(!bd*9_14x1+7&v52yq$f39wo(6w$yqwr#!)el^9-2!#=htr'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+deploy = False
+if(deploy):
+    # deploy
+    SECRET_KEY = os.getenv('SECRET_KEY', 'LIARA_URL is not set.')
+    shop_email = os.getenv('EMAIL_HOST', 'LIARA_URL is not set.')
+    password_email = os.getenv('EMAIL_HOST_PASSWORD', 'LIARA_URL is not set.')
+    merchant = os.getenv('MERCHANT', 'LIARA_URL is not set.')
+    DEBUG = os.getenv('DEBUG', 'LIARA_URL is not set.')
+    admin_url = os.getenv('ADMIN', 'LIARA_URL is not set.')
+    ip_find = os.getenv('IPFIND', 'LIARA_URL is not set.')
+else:
+    # local
+    SECRET_KEY = config('SECRET_KEY')
+    # shop_email = config('EMAIL_HOST')
+    # password_email = config('EMAIL_HOST_PASSWORD')
+    # merchant = config('MERCHANT')
+    admin_url = config('ADMIN')
+    # ip_find = config('IPFIND')
+    DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -22,6 +33,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+INSTALLED_APPS += [
+    'accounts.apps.AccountsConfig',
+    'blog.apps.BlogConfig',
+    'cart.apps.CartConfig',
+    'layout.apps.LayoutConfig',
+    'orders.apps.OrdersConfig',
+    'products.apps.ProductsConfig',
 ]
 
 MIDDLEWARE = [
@@ -58,12 +78,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if(deploy):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('database_name', 'LIARA_URL is not set.'),
+            'USER': os.getenv('database_username', 'LIARA_URL is not set.'),
+            'PASSWORD': os.getenv('password', 'LIARA_URL is not set.'),
+            'HOST': os.getenv('database_hostname_or_ip', 'LIARA_URL is not set.'),
+            'PORT': os.getenv('database_port', 'LIARA_URL is not set.'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
