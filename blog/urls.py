d6e741ec1bf2ccb31812c30197d92c18f2f388cc
unknown <aidinzaeim@gmail.com>
2024-02-11 01:50:48 -0800

    Category CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('article/all/', ArticleListView.as_view(),name="article_list"),
     path('article/update/<int:pk>/', ArticleUpdateView.as_view(),name="article_update"),
     path('article/delete/<int:pk>/', ArticleDeleteAPIView.as_view(),name="article_delete"),
+    path('category/', CategoryCreateAPIView.as_view(),name="Category_create"),
+    path('category/<int:pk>/', CategoryDetailAPIVeiw.as_view(),name="Category_read"),    
+    path('category/all/', CategoryListAPIView.as_view(),name="Category_list"),
+    path('category/update/<int:pk>/', CategoryUpdateAPIVeiw.as_view(),name="Category_update"),
+    path('category/delete/<int:pk>/', CategoryDeleteAPIVew.as_view(),name="Category_delete"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,11 @@ from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework import status
 from rest_framework.views import APIView
-from .serializers import ArticleSerializer
-from .models import Article
+from .serializers import ArticleSerializer, CategorySerializer
+from .models import Article, Category
 
+
+#Article API views
 
 class ArticleCreateAPIView(APIView):    
     """Creating an Article"""
@@ -19,17 +21,17 @@ class ArticleCreateAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 #---------------------------
 class ArticleDetailView(generics.RetrieveAPIView):
-    """Getting the details of an Article with ID"""
+    """Getting the details of an Article with ID(domain.com/..../pk/)"""
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer    
 #---------------------------
 class ArticleUpdateView(generics.UpdateAPIView):
-    """Updating the informations of an Article with ID"""
+    """Updating the informations of an Article with ID(domain.com/..../pk/)"""
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer    
 #---------------------------
 class ArticleDeleteAPIView(generics.DestroyAPIView):
-    """Deleting an Article with ID"""
+    """Deleting an Article with ID(domain.com/..../pk/)"""
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 #---------------------------
@@ -37,3 +39,37 @@ class ArticleListView(generics.ListAPIView):
     """Listing all of the Articles"""
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer    
+#---------------------------
+    
+#Categoty API views
+
+class CategoryCreateAPIView(APIView):
+    """Creating a Category"""
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'دسته جدید ساخته شد', 'data': serializer.data})
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#---------------------------
+class CategoryDetailAPIVeiw(generics.RetrieveAPIView):
+    """Getting the details of a Category with ID(domain.com/..../pk/)"""
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+#---------------------------
+class CategoryListAPIView(generics.ListAPIView):    
+    """Listing all of the Categories"""
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+#---------------------------
+class CategoryUpdateAPIVeiw(generics.UpdateAPIView):
+    """Updating the informations of a Category with ID(domain.com/..../pk/)"""    
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+#---------------------------
+class CategoryDeleteAPIVew(generics.DestroyAPIView):
+    """Deleting a Category with ID(domain.com/..../pk/)"""    
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+#---------------------------
