a6749659b0079a03b9ae782dc91ea046b00218be
taa <TahaM8000@gmail.com>
2024-02-29 12:54:38 +0330

    test model complete

--- cart/tests/test_models.py
+++ cart/tests/test_models.py
@@ -1,3 +1,4 @@
+from itertools import product
 from django.test import TestCase
 from ..models import *
 from django.utils import timezone
@@ -8,4 +9,33 @@ class TestCouponModel(TestCase):
         coupon = Coupon.objects.create(code='Iust', valid_from=timezone.now().date(), 
                         valid_to=timezone.now().date()+ timedelta(days=1), discount=25)
         self.assertEqual(str(coupon),'25-Iust')
+#---------------------------
+class TestCartModel(TestCase):
+    def test_model_str(self):
+        user = User(
+                phoneNumber = '09404016386',
+                email = f"09404016386@gmail.com",
+            )
+        user.id = 200
+        user.save()
+        cart = Cart.objects.create(user=user, )
+        self.assertEqual(str(cart),f'{user} - {cart.id}')
+#---------------------------
+class TestCartItemModel(TestCase):
+    def test_model_str(self):
+        user = User(
+                phoneNumber = '09404016386',
+                email = f"09404016386@gmail.com",
+            )
+        user.id = 200
+        user.save()
+        cart = Cart.objects.create(user=user, )
+
+        cart_item = CartItem(cart=cart)
+        
+
+
+
+        self.assertEqual(str(cart),f'{user} - {cart.id}')
+
 #---------------------------
