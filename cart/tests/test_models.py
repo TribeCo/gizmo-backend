fd45061f8f19d26bee0bf0efdd66503a32e690d7
taa <TahaM8000@gmail.com>
2024-02-29 11:26:17 +0330

    test models coupon

--- cart/tests/test_models.py
+++ cart/tests/test_models.py
@@ -1,2 +1,11 @@
 from django.test import TestCase
+from ..models import *
+from django.utils import timezone
+from datetime import timedelta
+#---------------------------
+class TestCouponModel(TestCase):
+    def test_model_str(self):
+        coupon = Coupon.objects.create(code='Iust', valid_from=timezone.now().date(), 
+                        valid_to=timezone.now().date()+ timedelta(days=1), discount=25)
+        self.assertEqual(str(coupon),'25-Iust')
 #---------------------------
