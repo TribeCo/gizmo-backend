254d7305b9315873550f6e04d461af4fc75351ef
taa <TahaM8000@gmail.com>
2024-03-07 19:21:29 +0330

    order section

---
+++ orders/api/user_orders_apies.py
@@ -0,0 +1,50 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from ..serializers import *
+from ..models import Product
+from inquiry.models import ForeignOrder
+from orders.models import Order
+from products.serializers import *
+from rest_framework.permissions import IsAuthenticated
+#---------------------------
+"""
+    The codes related to the site's user orders are in this app.
+    api's in user_orders_apies.py :
+
+    1- ListUserOrdersAPIView --> Lists all orders of user 
+
+
+"""
+#---------------------------
+messages_for_front = {
+    'user_created' : 'کاربر جدید ایجاد شد.',
+    'user_not_found' : 'کاربر یافت نشد',
+    'article_not_found' : 'مقاله یافت نشد',
+    'product_not_found' : 'محصول یافت نشد',
+    'comment_not_found' : 'نظر یافت نشد',
+    'comment_deleted' : 'نظر حذف شد',
+    'user_found' : 'کاربر یافت شد.',
+    'password_changed' : 'پسورد با موفقیت تغییر کرد.',
+    'wrong_coode' : 'کد اعتبارسنجی نامعتبر است.',
+    'right_code' : 'کد اعتبارسنجی صحیح است.',
+    'code_sent' : 'کد ارسال شد.',
+    'favorite_products_not_found': 'محصول مورد علاقه ای وجود ندارد',
+    'product_added_to_wishlist': 'محصول به لیست مورد علاقه‌ها اضافه شد',
+    'product_removed_from_wishlist': 'محصول از لیست مورد علاقه‌ها حذف شد',
+    'not_id' : 'آیدی مورد نیاز است.',
+    'product_removed_from_informing' : 'محصول از لیست انتظار حذف شد.',
+    'product_added_to_informing' : 'محصول به لیست انتظار اضافه شد.',
+    
+}
+#---------------------------
+class ListUserOrdersAPIView(APIView):
+    """Lists all orders of user """
+    permission_classes = [IsAuthenticated]
+    def get(self,request):
+        orders = request.user.orders
+        
+        serializer = OrderItemsSerializer(orders, many=True)
+
+        return Response({'data': serializer.data},status=status.HTTP_200_OK)
+#---------------------------
--- orders/views.py
+++ orders/views.py
@@ -1,2 +1,3 @@
 from .api.orders_apis import *
+from .api.user_orders_apies import *
 #---------------------------
