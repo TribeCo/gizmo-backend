c3b8e5651cf6714f47b1159b0d146e3edefbe735
taa <TahaM8000@gmail.com>
2024-03-21 21:49:56 +0330

    update portal

--- cart/models.py
+++ cart/models.py
@@ -36,7 +36,7 @@ class Cart(models.Model):
     def total_price(self):
         total = sum(item.get_cost() for item in self.items.all())
         if self.discount:
-            discount_price = (self.discount/100)* total
+            discount_price = (self.coupon.discount/100)* total
             return total - discount_price
         return total
 
