391bc341bd3c9abe72dcbbb64563edae6b5b893b
taa <TahaM8000@gmail.com>
2024-04-25 15:22:43 +0330

    Local storage

--- cart/api/cart_api.py
+++ cart/api/cart_api.py
@@ -36,6 +36,14 @@ class CartDetailAPIView(APIView):
         serializer = CartSerializer(request.user.cart)
         return Response({'cart':serializer.data}, status=status.HTTP_200_OK)
 #---------------------------
+class CartDetailLocalAPIView(APIView):
+    """Getting the user's shopping cart information"""
+    serializer_class = CartLocalSerializer
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        serializer = CartLocalSerializer(request.user.cart)
+        return Response({'cart':serializer.data}, status=status.HTTP_200_OK)
+#---------------------------
 class AddProductToCartAPIView(APIView):
     """
         Add a product to the cart
